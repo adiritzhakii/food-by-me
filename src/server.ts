f80877dfafbf57f8516e51526241d4372eea0e9f
adir <adir@gmail.com>
2024-12-28 16:59:04 +0200

    Auth&Post - Add MVC and implement tests. initial express server

---
+++ src/server.ts
@@ -0,0 +1,52 @@
+import express, { Express } from "express";
+const app = express();
+import dotenv from "dotenv";
+dotenv.config();
+import mongoose from "mongoose";
+import bodyParser from "body-parser";
+import postsRoute from "./routes/posts_route";
+import authController from "./routes/auth_route";
+import swaggerJsDoc from "swagger-jsdoc";
+import swaggerUI from "swagger-ui-express";
+
+const db = mongoose.connection;
+db.on("error", (error) => console.error(error));
+db.once("open", () => console.log("Connected to Database"));
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use("/posts", postsRoute);
+app.use("/auth", authController);
+
+app.get("/about", (req, res) => {
+  res.send("Hello World!");
+});
+
+const options = {
+  definition: {
+    openapi: "3.0.0",
+    info: {
+      title: "Web Dev 2025 - D - REST API",
+      version: "1.0.0",
+      description: "REST server including authentication using JWT",
+    },
+    servers: [{ url: "http://localhost:" + process.env.PORT, },],
+  },
+  apis: ["./src/routes/*.ts"],
+};
+const specs = swaggerJsDoc(options);
+app.use("/api-docs", swaggerUI.serve, swaggerUI.setup(specs));
+
+const initApp = () => {
+  return new Promise<Express>(async (resolve, reject) => {
+    if (process.env.DB_CONNECTION == undefined) {
+      reject("DB_CONNECTION is not defined");
+    } else {
+      await mongoose.connect(process.env.DB_CONNECTION);
+      resolve(app);
+    }
+  });
+};
+
+export default initApp;
